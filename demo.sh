44106e90415bb74760689ce6b3acc9c8655abe5a
João Pedro <joaomachado@biofy.tech>
2025-09-30 16:10:50 -0300

    adding new sh files to automatizate train and test and a example with comparation with models

---
+++ demo.sh
@@ -0,0 +1,4 @@
+python train_fn.py \
+  --infer "Qual é a descrição do produto 'iPhone 12 128GB'?" \
+  --use_trained ./models/flan_t5_a10_lora && \
+python base_inference.py --limit 3 && python trained_inference.py --limit 3
